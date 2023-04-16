4fa3d199e315718444682205ac47b66034a0f92d
Joyce Villarroel Mascarenhas <joycevillarroelmascarenhas@MacBook-Air-de-Joyce.local>
2023-04-16 15:40:37 -0300

    task: creation of the base of routes and standard layouts (page with header or without)

---
+++ src/layouts/DefaultLayout/DefaultLayout.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`
+
+export const LayoutContent = styled.div`
+  width: 100%;
+  max-width: 75.9375rem;
+  height: calc(100vh - 14.875rem);
+  padding: 0 1rem;
+`
