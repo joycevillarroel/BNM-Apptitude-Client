197fa6a4a5caa94067edd7969c35090eb4721a27
Joyce Villarroel Mascarenhas <joycevillarroelmascarenhas@MacBook-Air-de-Joyce.local>
2023-04-18 07:30:57 -0300

    task: Creating the MetricsTable component

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -5,31 +5,7 @@ export const HomeContainer = styled.div`
   height: 100%;
 `
 
-export const SessionButtons = styled.div`
-  width: 100%;
-  height: 4rem;
-  display: flex;
-  align-items: flex-start;
-  background-color: ${({ theme }) => theme.black};
-  align-items: self-end;
-  padding-left: 1rem;
-`
-
-export const SessionButton = styled.div`
-  margin-left: 1rem;
-`
-
-export const SessionCards = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: flex-start;
-  padding: 2rem;
-  align-items: space-between;
-  justify-content: space-between;
-  flex-flow: row wrap;
-`
-
 export const MessageText = styled.text`
   color: ${({ theme }) => theme.secondary};
+  margin: 16px;
 `
